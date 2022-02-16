1fa77aa7c2419475d0ba76e86148390d560a8268
João Vitor <jovi.oli04@gmail.com>
2022-02-16 12:53:49 -0300

    Modificação no CategorySelectButton

--- gofinances/src/screens/CategorySelect/style.ts
+++ gofinances/src/screens/CategorySelect/style.ts
@@ -17,7 +17,7 @@ export const Header = styled.View`
 `;
 export const Title = styled.Text`
     font-family: ${({ theme }) => theme.fonts.regular};
-    font-size: ${RFValue(14)}px;
+    font-size: ${RFValue(18)}px;
     color: ${({ theme }) => theme.colors.shape};
 
 `;
