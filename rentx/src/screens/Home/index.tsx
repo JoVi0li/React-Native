f93309bb41ffa63103102edbee60af9198008587
JoViOli <jovi.oli04@gmail.com>
2022-03-03 13:33:57 -0300

    Header implementado

---
+++ rentx/src/screens/Home/index.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import {
+    Container,
+    Header,
+    TotalCars,
+    HeaderContent,
+} from "./style";
+
+export function HomeScreen() {
+    return (
+        <Container>
+            <StatusBar
+                barStyle='light-content'
+                backgroundColor="transparent"
+                translucent={true}
+            />
+            <Header>
+                <HeaderContent>
+                    <Logo
+                        width={RFValue(108)}
+                        height={RFValue(12)}
+                    />
+                    <TotalCars>
+                        Total de 12 carros
+                    </TotalCars>
+                </HeaderContent>
+            </Header>
+        </Container>
+    )
+}
